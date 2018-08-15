c3f3e5cd28500e9706ac97317b5136d4e3f714e9
michaelost <mykhailo.ostrovyi@bytebrand.net>
2018-08-15 22:05:05 +0300

    set up babel generators

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 var axios = require('axios');
 
-axios.get('https://api.github.com/users/michaelost/followers').then(function (response) {
-console.log('response.data', response.data);
-  const followers = response.data
-    .map(f => {
+
+(async function () {
+  var getGithubUsers = async function (userName) {
+    const response = await axios.get('https://api.github.com/users/michaelost/followers');
+    const followers = response.data.map(f => {
       return {
         login: f.login,
         id: f.id,
         followers: f.followers_url,
       }
     })
-  console.log('followers', followers);
-})
+    return followers;
+  }
 
+  var users = await getGithubUsers();
 
+  console.log('users', users);
+
+})()
 
