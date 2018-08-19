a5c491c13c16bd9b556e3607e197c0ce8eab07c0
michaelost <mykhailo.ostrovyi@bytebrand.net>
2018-08-20 00:10:50 +0300

    write some tips

--- abra.js
+++ abra.js
@@ -10,3 +10,23 @@ var assert = function (val1, val2) {
 
 var newString = removeChar('abrakadabra', 'k');
 console.log(assert(newString, 'abraadabra'))
+
+
+
+var characters = ['a', 'b', 'c', 'd', 'e'];
+var currentIndex = 0;
+
+
+function removeNtimes(count, string) {
+  var charToRemove = characters[currentIndex];
+  //  while string.length > 0 ||  count > 0
+  //  do :
+    if ( /* characted is in string */ ) {
+      // remove character from string
+    } else {
+      // increment currentIndex 
+      // try to remove the next char from string
+    }
+//    count = count - 1;
+}
+
