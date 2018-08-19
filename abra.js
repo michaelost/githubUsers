4f508db1a7cc4257d29c612f7904bf1b9ecf8937
uliaka <uliana.gorin95@gmail.com>
2018-08-20 00:03:27 +0300

    removing works

--- abra.js
+++ abra.js
@@ -1,21 +1,12 @@
-
-
-
 function removeChar (str, charToRemove) {
   var index = str.indexOf(charToRemove);
-  return str.substring(0, index) + str.substring(index, srt.length);
-
+  var result = str.substring(0, index) + str.substring(index + 1, str.length);
+  return result;
 }
-
-
-
-var newString = removeChar('abrakadabra', 'k');
-
-
-
-
-
 
 var assert = function (val1, val2) {
   return val1 === val2;
 }
+
+var newString = removeChar('abrakadabra', 'k');
+console.log(assert(newString, 'abraadabra'))
