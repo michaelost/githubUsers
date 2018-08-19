92e2620cf114022961e873f0301029cb5a5e67b0
michaelost <mykhailo.ostrovyi@bytebrand.net>
2018-08-19 23:47:38 +0300

    add assertion function

--- abra.js
+++ abra.js
@@ -1,5 +1,21 @@
+
+
+
 function removeChar (str, charToRemove) {
 
 // return  str without charToRemove
 
+}
+
+
+
+var newString = removeChar('abrakadabra', 'k');
+
+
+
+
+
+
+var assert = function (val1, val2) {
+  return val1 === val2;
 }
